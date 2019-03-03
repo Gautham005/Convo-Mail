d762566db912ce9f8e0f3306bcdefdad86a2d98a
rahulk98 <rahul.krishnan27@gmail.com>
2019-03-03 16:15:25 +0530

    view inbox

--- app/src/main/java/com/example/convomail/EmailList.java
+++ app/src/main/java/com/example/convomail/EmailList.java
@@ -2,6 +2,7 @@ package com.example.convomail;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,7 +11,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Vector;
@@ -18,6 +21,7 @@ import java.util.Vector;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
 
@@ -28,14 +32,13 @@ public class EmailList extends AppCompatActivity {
     static Vector<String> dateHeader;
     static Vector<String> fromHeader;
     static Vector<String> subjectHeader;
-    static ArrayList<String> header;
+    static ArrayList<String> header = new ArrayList<String>();
     static int EmailNumber;
     String body = "";
     private Login login;
     Intent newIntent;
-
-    private ListView list = null;
-    private ArrayAdapter<String> adapter=null;
+    private ListView list ;
+     private ArrayAdapter<String> adapter=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,16 +47,14 @@ public class EmailList extends AppCompatActivity {
         String password = newIntent.getStringExtra("pass");
         String username = newIntent.getStringExtra("username");
         String name = newIntent.getStringExtra("Name");
-        connectServer(username, password);
+
         login = new Login(username, password, name);
+        connectServer(login);
 //        pl = new PersistLogins(login);
 //        pl.serializeOut(this);
-        adapter = new ArrayAdapter<String>(this, R.layout.dataview, header);
-        list = (ListView) this.findViewById(R.id.ListView);
-        if(adapter!=null){
-            list.setAdapter(adapter);
-            list.setTextFilterEnabled(true);
-        }
+            Log.d("null", header.size()+" ");
+
+
 
 //        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            public void onItemClick(AdapterView<?> parent, View view,
@@ -73,19 +74,84 @@ public class EmailList extends AppCompatActivity {
         this.finish();
     }
 
-    public static void connectServer(String userNameIn, String passwordIn) {
+    public  void connectServer(Login login) {
         try {
-            String username = userNameIn;
-            String pass = passwordIn;
-            doit(username, pass);
 
-
-
+            String host = "pop.gmail.com";
+            String mailStoretype = "pop3";
+            new RetrieveMessages().execute(login.getUserID(), login.getPassword());
         }
         catch(Exception e){}
 
 
     }
+    public void setHeader(ArrayList<String> h){
+        header = h;
+        Log.d("size", h.get(0));
+        adapter = new ArrayAdapter<String>(this, R.layout.dataview, R.id.TextView ,header);
+        list = (ListView) this.findViewById(R.id.ListView);
+        list.setAdapter(adapter);
+
+
+    }
+    public static void fetch(String pop3Host, String storeType, String user,
+                             String password) {
+        try {
+            // create properties field
+            Properties properties = new Properties();
+            properties.put("mail.store.protocol", "pop3");
+            properties.put("mail.pop3.host", pop3Host);
+            properties.put("mail.pop3.port", "995");
+            properties.put("mail.pop3.starttls.enable", "true");
+            Session emailSession = Session.getDefaultInstance(properties);
+            // emailSession.setDebug(true);
+
+            // create the POP3 store object and connect with the pop server
+            Store store = emailSession.getStore("pop3s");
+
+            store.connect(pop3Host, user, password);
+
+            // create the folder object and open it
+            Folder emailFolder = store.getFolder("INBOX");
+            emailFolder.open(Folder.READ_ONLY);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    System.in));
+
+            // retrieve the messages from the folder in an array and print it
+            Message[] messages = emailFolder.getMessages();
+            String tempDate, tempSubject, tempHeader, tempFrom;
+            for (int i = 0; i < messages.length; i++) {
+                tempDate = "";
+                tempSubject = "";
+                tempHeader = "";
+                tempDate = messages[i].getSentDate().toString();
+                tempSubject = messages[i].getSubject().toString();
+                tempFrom = messages[i].getFrom().toString();
+                tempHeader = tempDate + "\n" + tempSubject + "\n" + tempFrom;
+                Log.d("header", tempHeader);
+//                header.add(tempHeader);
+                System.out.print(tempHeader);
+            }
+//            for (int i = 0; i < messages.length; i++) {
+//                Message message = messages[i];
+//                System.out.println("---------------------------------");
+//                writePart(message);
+//                String line = reader.readLine();
+//                if ("YES".equals(line)) {
+//                    message.writeTo(System.out);
+//                } else if ("QUIT".equals(line)) {
+//                    break;
+//                }
+//            }
+
+            // close the store and folder objects
+            emailFolder.close(false);
+            store.close();
+
+        } catch (Exception e) {
+            Log.d("error", e.toString()) ;
+        }    }
     public static Message[] doit(String username, String password) throws MessagingException, IOException {
         Folder folder = null;
         Store store = null;
@@ -167,4 +233,68 @@ public class EmailList extends AppCompatActivity {
         //a.putExtra("body", "HEY");
         startActivity(a);
     }
+    class RetrieveMessages extends AsyncTask<String, Void, ArrayList<String>>{
+
+
+        @Override
+        protected ArrayList<String> doInBackground(String... strings) {
+            ArrayList<String> header = new ArrayList<String>();
+            try{
+                // create properties field
+                String pop3Host = "pop.gmail.com";
+                Properties properties = new Properties();
+                properties.put("mail.store.protocol", "pop3");
+                properties.put("mail.pop3.host", pop3Host);
+                properties.put("mail.pop3.port", "995");
+                properties.put("mail.pop3.starttls.enable", "true");
+                Session emailSession = Session.getDefaultInstance(properties);
+                // emailSession.setDebug(true);
+
+                // create the POP3 store object and connect with the pop server
+                Store store = emailSession.getStore("pop3s");
+                store.connect(pop3Host, strings[0], strings[1]);
+
+                // create the folder object and open it
+                Folder emailFolder = store.getFolder("INBOX");
+                emailFolder.open(Folder.READ_ONLY);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        System.in));
+
+                // retrieve the messages from the folder in an array and print it
+                Message[] messages = emailFolder.getMessages();
+                String tempDate, tempSubject, tempHeader, tempFrom;
+                for (int i = 0; i < messages.length; i++) {
+                    tempDate = "";
+                    tempSubject = "";
+                    tempHeader = "";
+                    tempDate = messages[i].getSentDate().toString();
+                    tempSubject = messages[i].getSubject().toString();
+                    tempFrom = messages[i].getFrom()[0].toString();
+                    tempHeader = tempDate + "\n" + tempSubject + "\n" + tempFrom;
+                    Log.d("header", tempHeader);
+
+                    System.out.print(tempHeader);
+                    header.add(tempHeader);
+                }
+                if(emailFolder!=null){
+                    emailFolder.close(false);
+                }
+                if(store!=null){
+                    store.close();
+                }
+
+            }
+            catch (Exception e){
+                Log.d("err", e.toString()) ;
+            }
+            return header;
+        }
+
+        protected  void onPostExecute(ArrayList<String> strings) {
+            setHeader(strings);
+        }
+    }
+
 }
+
