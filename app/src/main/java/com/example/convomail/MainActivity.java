116113b6ef2add47ec44995055ac09f201325f8c
rahulk98 <rahul.krishnan27@gmail.com>
2019-05-01 23:03:47 +0530

    fixes

--- app/src/main/java/com/example/convomail/MainActivity.java
+++ app/src/main/java/com/example/convomail/MainActivity.java
@@ -138,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
         a.addCategory(Intent.CATEGORY_HOME);
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
+
     }
 
     public void setAuth(Boolean b) {
